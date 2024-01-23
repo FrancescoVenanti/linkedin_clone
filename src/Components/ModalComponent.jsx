06ad94dd3b9a08f0d94b7884065f8bf4937a64c2
FrancescoVenanti <francesco.venanti@gmail.com>
2024-01-23 13:06:47 +0100

    sgdffg

--- src/Components/ModalComponent.jsx
+++ src/Components/ModalComponent.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
@@ -6,162 +6,222 @@ import { useSelector } from "react-redux";
 import { token } from "../token";
 
 function ModalComponent(props) {
-    const user = useSelector((state) => state.me.meData);
-    const experienceEndpoint = `https://striveschool-api.herokuapp.com/api/profile/${user._id}/experiences`;
-    const startYear = 1960;
-    const endYear = 2023;
-    const years = Array.from({ length: endYear - startYear + 1 }, (_, index) => startYear + index);
+	const user = useSelector((state) => state.me.meData);
+	const experienceEndpoint = `https://striveschool-api.herokuapp.com/api/profile/${user._id}/experiences`;
+	const startYear = 1960;
+	const endYear = 2023;
+	const years = Array.from({ length: endYear - startYear + 1 }, (_, index) => startYear + index);
+	const [startingTime, setStartingTime] = useState({ year: "", month: "" });
 
-    const [stillWorking, setStillWorking] = useState(true);
+	const [stillWorking, setStillWorking] = useState(true);
 
-    /* const [newExperience, setNewExperience] = useState({
-        role: "Full Stack Web Developer",
-        company: "FizzBuzz",
-        startDate: "2022-06-16",
-        endDate: "2023-06-16",
-        description: "Implementing new features",
-        area: "Milan",
-    });
+	const [newExperience, setNewExperience] = useState({
+		role: "Full Stack Web Developer",
+		company: "FizzBuzz",
+		startDate: "2022-06-16",
+		endDate: "2023-06-16",
+		description: "Implementing new features",
+		area: "Milan",
+	});
 
-    const postNewExperience = async () => {
-        try {
-            const resp = await fetch(experienceEndpoint, {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(newExperience),
-            });
+	const postNewExperience = async () => {
+		try {
+			const resp = await fetch(experienceEndpoint, {
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newExperience),
+			});
 
-            if (resp.ok) {
-                props.handleSetExperience();
-            } else {
-                console.error("Errore durante la richiesta POST:", resp.statusText);
-            }
-        } catch (error) {
-            console.error("Errore durante la richiesta POST:", error);
-        }
-    };
+			if (resp.ok) {
+				props.handleSetExperience();
+			} else {
+				console.error("Errore durante la richiesta POST:", resp.statusText);
+			}
+		} catch (error) {
+			console.error("Errore durante la richiesta POST:", error);
+		}
+	};
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        postNewExperience();
-    }; */
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		postNewExperience();
+	};
+	useEffect(() => {
+		const startingTimeString = startingTime.year.toString() + "-" + startingTime.month.toString() + "-01";
+		console.log(newExperience);
+		setNewExperience({
+			...newExperience,
+			startDate: startingTimeString,
+		});
+	}, [startingTime]);
 
-    return (
-        <Modal show={props.show} onHide={props.handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title
-                /* onChange={(e) => setNewExperience(...newExperience, (newExperience.role = e.target.value))} */
-                >
-                    Add Experience
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-3" controlId="title">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" placeholder="example Retail Sales Manager" required />
-                    </Form.Group>
+	return (
+		<Modal show={props.show} onHide={props.handleClose}>
+			<Modal.Header closeButton>
+				<Modal.Title>Add Experience</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<Form>
+					<Form.Group className="mb-3" controlId="title">
+						<Form.Label>Title</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="example Retail Sales Manager"
+							required
+							onChange={(e) =>
+								setNewExperience({
+									...newExperience,
+									role: e.target.value,
+								})
+							}
+						/>
+					</Form.Group>
 
-                    <Form.Group className="mb-3" controlId="company">
-                        <Form.Label>Company Name</Form.Label>
-                        <Form.Control type="text" placeholder="Example Microsoft" required />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="location">
-                        <Form.Label>Location</Form.Label>
-                        <Form.Control type="text" placeholder="Example London, UK" />
-                    </Form.Group>
+					<Form.Group className="mb-3" controlId="company">
+						<Form.Label>Company Name</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Example Microsoft"
+							required
+							onChange={(e) =>
+								setNewExperience({
+									...newExperience,
+									company: e.target.value,
+								})
+							}
+						/>
+					</Form.Group>
+					<Form.Group className="mb-3" controlId="location">
+						<Form.Label>Location</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Example London, UK"
+							onChange={(e) =>
+								setNewExperience({
+									...newExperience,
+									area: e.target.value,
+								})
+							}
+						/>
+					</Form.Group>
 
-                    <Form.Group className="mb-3" controlId="description">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" placeholder="write something" />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Check aria-label="I am currently working in this role">
-                            <Form.Check.Input type="checkbox" onChange={() => setStillWorking(!stillWorking)} />
-                            <Form.Check.Label>I am currently working in this role</Form.Check.Label>
-                        </Form.Check>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="start" required>
-                        <Form.Label>Starting date</Form.Label>
-                        <Row>
-                            <Col xs={12} md={6}>
-                                <Form.Select aria-label="month">
-                                    <option>Month</option>
-                                    <option value="1">January</option>
-                                    <option value="2">February</option>
-                                    <option value="3">March</option>
-                                    <option value="4">April</option>
-                                    <option value="5">May</option>
-                                    <option value="6">June</option>
-                                    <option value="7">July</option>
-                                    <option value="8">August</option>
-                                    <option value="9">September</option>
-                                    <option value="10">October</option>
-                                    <option value="11">November</option>
-                                    <option value="12">December</option>
-                                </Form.Select>
-                            </Col>
-                            <Col xs={12} md={6}>
-                                <Form.Select aria-label="year">
-                                    <option>Year</option>
-                                    {years.map((year, index) => (
-                                        <option key={index} value={index + 1}>
-                                            {year}
-                                        </option>
-                                    ))}
-                                </Form.Select>
-                            </Col>
-                        </Row>
-                    </Form.Group>
-                    {stillWorking && (
-                        <Form.Group className="mb-3" controlId="start" required>
-                            <Form.Label>Ending date</Form.Label>
-                            <Row>
-                                <Col xs={12} md={6}>
-                                    <Form.Select aria-label="month">
-                                        <option>Month</option>
-                                        <option value="1">January</option>
-                                        <option value="2">February</option>
-                                        <option value="3">March</option>
-                                        <option value="4">April</option>
-                                        <option value="5">May</option>
-                                        <option value="6">June</option>
-                                        <option value="7">July</option>
-                                        <option value="8">August</option>
-                                        <option value="9">September</option>
-                                        <option value="10">October</option>
-                                        <option value="11">November</option>
-                                        <option value="12">December</option>
-                                    </Form.Select>
-                                </Col>
-                                <Col xs={12} md={6}>
-                                    <Form.Select aria-label="year">
-                                        <option>Year</option>
-                                        {years.map((year, index) => (
-                                            <option key={index} value={index + 1}>
-                                                {year}
-                                            </option>
-                                        ))}
-                                    </Form.Select>
-                                </Col>
-                            </Row>
-                        </Form.Group>
-                    )}
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={() => props.handleClose()}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={() => props.handleClose()}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+					<Form.Group className="mb-3" controlId="description">
+						<Form.Label>Description</Form.Label>
+						<Form.Control
+							as="textarea"
+							placeholder="write something"
+							onChange={(e) =>
+								setNewExperience({
+									...newExperience,
+									description: e.target.value,
+								})
+							}
+						/>
+					</Form.Group>
+					<Form.Group>
+						<Form.Check aria-label="I am currently working in this role">
+							<Form.Check.Input type="checkbox" onChange={() => setStillWorking(!stillWorking)} />
+							<Form.Check.Label>I am currently working in this role</Form.Check.Label>
+						</Form.Check>
+					</Form.Group>
+					<Form.Group
+						className="mb-3"
+						controlId="start"
+						required
+						onChange={(e) => {
+							console.log(e.target.value);
+						}}
+					>
+						<Form.Label>Starting date</Form.Label>
+						<Row>
+							<Col xs={12} md={6}>
+								<Form.Select
+									aria-label="month"
+									onChange={(e) => {
+										setStartingTime({ ...startingTime, month: e.target.value });
+									}}
+								>
+									<option>Month</option>
+									<option value="01">January</option>
+									<option value="02">February</option>
+									<option value="03">March</option>
+									<option value="04">April</option>
+									<option value="05">May</option>
+									<option value="06">June</option>
+									<option value="07">July</option>
+									<option value="08">August</option>
+									<option value="09">September</option>
+									<option value="10">October</option>
+									<option value="11">November</option>
+									<option value="12">December</option>
+								</Form.Select>
+							</Col>
+							<Col xs={12} md={6}>
+								<Form.Select
+									aria-label="year"
+									onChange={(e) => {
+										setStartingTime({ ...startingTime, year: e.target.value });
+									}}
+								>
+									<option>Year</option>
+									{years.map((year, index) => (
+										<option key={index} value={year}>
+											{year}
+										</option>
+									))}
+								</Form.Select>
+							</Col>
+						</Row>
+					</Form.Group>
+					{stillWorking && (
+						<Form.Group className="mb-3" controlId="start" required>
+							<Form.Label>Ending date</Form.Label>
+							<Row>
+								<Col xs={12} md={6}>
+									<Form.Select aria-label="month">
+										<option>Month</option>
+										<option value="01">January</option>
+										<option value="02">February</option>
+										<option value="03">March</option>
+										<option value="04">April</option>
+										<option value="05">May</option>
+										<option value="06">June</option>
+										<option value="07">July</option>
+										<option value="08">August</option>
+										<option value="09">September</option>
+										<option value="10">October</option>
+										<option value="11">November</option>
+										<option value="12">December</option>
+									</Form.Select>
+								</Col>
+								<Col xs={12} md={6}>
+									<Form.Select aria-label="year">
+										<option>Year</option>
+										{years.map((year, index) => (
+											<option key={index} value={year}>
+												{year}
+											</option>
+										))}
+									</Form.Select>
+								</Col>
+							</Row>
+						</Form.Group>
+					)}
+				</Form>
+			</Modal.Body>
+			<Modal.Footer>
+				<Button variant="secondary" onClick={() => props.handleClose()}>
+					Close
+				</Button>
+				<Button variant="primary" onClick={() => props.handleClose()}>
+					Save Changes
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
 }
 
 export default ModalComponent;
--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -6,49 +6,50 @@ export const GET_ME = "GET_ME";
 export const endpoint = "https://striveschool-api.herokuapp.com/api/profile/";
 
 export const getUsersAction = (type) => {
-    return async (dispatch) => {
-        try {
-            const resp = await fetch(endpoint, {
-                headers: {
-                    Authorization: `bearer ${token}`,
-                },
-            });
-            if (resp.ok) {
-                let result = await resp.json();
-                console.log(result);
-                dispatch({
-                    type: type,
-                    payload: result,
-                });
-            } else {
-                throw new Error("failed to fetch");
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
+	return async (dispatch) => {
+		try {
+			const resp = await fetch(endpoint, {
+				headers: {
+					Authorization: `bearer ${token}`,
+					mode: "no-cors",
+				},
+			});
+			if (resp.ok) {
+				let result = await resp.json();
+				console.log(result);
+				dispatch({
+					type: type,
+					payload: result,
+				});
+			} else {
+				throw new Error("failed to fetch");
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 };
 
 export const getMeAction = (query) => {
-    return async (dispatch) => {
-        try {
-            const resp = await fetch(endpoint + query, {
-                headers: {
-                    Authorization: `bearer ${token}`,
-                },
-            });
-            if (resp.ok) {
-                let result = await resp.json();
-                console.log(result);
-                dispatch({
-                    type: GET_ME,
-                    payload: result,
-                });
-            } else {
-                throw new Error("failed to fetch");
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
+	return async (dispatch) => {
+		try {
+			const resp = await fetch(endpoint + query, {
+				headers: {
+					Authorization: `bearer ${token}`,
+				},
+			});
+			if (resp.ok) {
+				let result = await resp.json();
+				console.log(result);
+				dispatch({
+					type: GET_ME,
+					payload: result,
+				});
+			} else {
+				throw new Error("failed to fetch");
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 };
