7feb5b2a3f8e22a6ee66f37dd9ec3e47e67b1f4e
mattia lugaro <mattialugaro@gmail.com>
2024-01-24 10:40:58 +0100

    home right scheletro

--- src/Components/HomeRight.jsx
+++ src/Components/HomeRight.jsx
@@ -1,7 +1,24 @@
+import { Button } from "react-bootstrap";
+import { InfoSquareFill, Plus } from "react-bootstrap-icons";
+
 const HomeRight = () => {
     return (
         <div className="bg-white rounded-3 border border-2">
-            <p>prova</p>
+            <div>
+                <p>Add to your feed</p>
+                <InfoSquareFill/>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <p>Name Group</p>
+                    <p>Company • (Hospitality)</p>
+                    <Button className="d-flex align-items-center hover-gray text-secodary" variant="none">
+                        <Plus />
+                        <span>Follow</span>
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
