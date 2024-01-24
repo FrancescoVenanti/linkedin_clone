d7b4e5582469ccce474cb2dd27a0d5c0d57fc759
mattia lugaro <mattialugaro@gmail.com>
2024-01-24 14:25:26 +0100

    home right mancano immagini azienda

--- src/Components/HomeRight.jsx
+++ src/Components/HomeRight.jsx
@@ -1,24 +1,52 @@
 import { Button } from "react-bootstrap";
-import { InfoSquareFill, Plus } from "react-bootstrap-icons";
+import { ArrowRight, InfoSquareFill, Plus } from "react-bootstrap-icons";
 
 const HomeRight = () => {
     return (
         <div className="bg-white rounded-3 border border-2">
-            <div>
-                <p>Add to your feed</p>
+            <div className="d-flex align-items-center p-2">
+                <p className="mb-0 me-auto">Add to your feed</p>
                 <InfoSquareFill/>
             </div>
-            <div>
-                <img src="" alt="" />
+            <div className="d-flex mb-2">
+                <img className="rounded-circle" src="" alt="logo azienda" />
                 <div>
-                    <p>Name Group</p>
-                    <p>Company • (Hospitality)</p>
-                    <Button className="d-flex align-items-center hover-gray text-secodary" variant="none">
+                    <p className="mb-0 fw-bold">Name Group</p>
+                    <p className="mb-0 text-secondary">Company • (Hospitality)</p>
+                    <Button className="d-flex align-items-center hover-gray text-secodary border border-2 border-secondary rounded-pill my-1" variant="none">
                         <Plus />
                         <span>Follow</span>
                     </Button>
                 </div>
             </div>
+            <div className="d-flex mb-2">
+                <img className="rounded-circle" src="" alt="logo azienda" />
+                <div>
+                    <p className="mb-0 fw-bold">Name Group</p>
+                    <p className="mb-0 text-secondary">Company • (Hospitality)</p>
+                    <Button className="d-flex align-items-center hover-gray text-secodary border border-2 border-secondary rounded-pill my-1" variant="none">
+                        <Plus />
+                        <span>Follow</span>
+                    </Button>
+                </div>
+            </div>
+            <div className="d-flex mb-2">
+                <img className="rounded-circle" src="" alt="logo azienda" />
+                <div>
+                    <p className="mb-0 fw-bold">Name Group</p>
+                    <p className="mb-0 text-secondary">Company • (Hospitality)</p>
+                    <Button className="d-flex align-items-center hover-gray text-secodary border border-2 border-secondary rounded-pill my-1" variant="none">
+                        <Plus />
+                        <span>Follow</span>
+                    </Button>
+                </div>
+            </div>
+            <div className="border-top p-2">
+				<a href="" className="text-black text-decoration-none hover-gray">
+					<span className="me-1">View all reccomandations</span>
+					<ArrowRight />
+				</a>
+			</div>
         </div>
     );
 };
